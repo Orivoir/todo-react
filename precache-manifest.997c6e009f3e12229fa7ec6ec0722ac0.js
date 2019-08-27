3c1bfb0fecb36faf061c045963ca0051c9230f9d
= <=>
2019-08-27 22:22:12 +0200

    Updates

---
+++ precache-manifest.997c6e009f3e12229fa7ec6ec0722ac0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70ad40a0940314ec7fea4d232a94b919",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "8a0171ba0e43ba74e2fd",
+    "url": "/todo-react/static/css/main.db4648fd.chunk.css"
+  },
+  {
+    "revision": "86e883fbddfa618ac90d",
+    "url": "/todo-react/static/js/2.c9e5932e.chunk.js"
+  },
+  {
+    "revision": "8a0171ba0e43ba74e2fd",
+    "url": "/todo-react/static/js/main.ac29d4b6.chunk.js"
+  },
+  {
+    "revision": "cddd87bd2191fd6a0603",
+    "url": "/todo-react/static/js/runtime~main.bfd59dd3.js"
+  }
+]);
